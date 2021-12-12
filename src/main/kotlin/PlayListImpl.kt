cad8ece5d8528a60da61781ac0b1316487442b23
ironslaughte <anm7590@gmail.com>
2021-12-12 15:24:23 +0300

    Изменения коснулись плейлиста, а именно его создания

--- src/main/kotlin/PlayListImpl.kt
+++ src/main/kotlin/PlayListImpl.kt
@@ -4,28 +4,36 @@ import dataClasses.Song
 class PlayListImpl(
     private val songs : MutableList<Song>,
     private val albums : MutableList<Album>,
-    private val playList : MutableList<Song> = mutableListOf()
 ) : Playlist {
+    private val playList : MutableList<Song> = mutableListOf()
+    init{
+        if(playList.isEmpty()){
+            // Создаем плейлист
+            albums.listIterator().let {
+                while(it.hasNext()){
+                    playList.addAll(it.next().songList)
+                }
+            }
+            songs.listIterator().let {
+                while(it.hasNext()){
+                    playList.add(it.next())
+                }
+            }
+        }
+    }
     override fun addSong(song : Song){
         songs.add(song)
+        // Обновляем плейлист
+        playList.add(song)
     }
 
     override fun addAlbum(album : Album) {
         albums.add(album)
+        // Обновляем плейлист
+        playList.addAll(album.songList)
+
     }
     override fun shuffle(){
-        // Добавляем все песни(из альбомов тоже) в один плейлист
-        albums.listIterator().let {
-            while(it.hasNext()){
-                playList.addAll(it.next().songList)
-            }
-        }
-        songs.listIterator().let {
-            while(it.hasNext()){
-                playList.add(it.next())
-            }
-        }
-        // После формирования плейлиста перемешиваем песни в нем
         playList.shuffle()
     }
     override fun toString(): String {
